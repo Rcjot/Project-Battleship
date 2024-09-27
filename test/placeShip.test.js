431d8359c8faed9a7ab6ce7013df87bb5aedab5f
Rcjot <rcappy495@gmail.com>
2024-09-27 14:14:41 +0800

    finished placeShip testing

--- test/placeShip.test.js
+++ test/placeShip.test.js
@@ -1,7 +1,24 @@
 const Ship = require("../src/ship");
 const Gameboard = require("../src/gameboard");
 
-test("place a ship, south", () => {
+test("place ship with different size", () => {
+    const myShip1 = Ship(1);
+    const myShip2 = Ship(2);
+    const myShip3 = Ship(3);
+    const myShip4 = Ship(4);
+    const myShip5 = Ship(5);
+    const myBoard = Gameboard();
+
+    expect(myBoard.placeShip(myShip1, [0, 0])).toBe(true);
+    expect(myBoard.placeShip(myShip2, [1, 0])).toBe(true);
+    expect(myBoard.placeShip(myShip3, [2, 0])).toBe(true);
+    expect(myBoard.placeShip(myShip4, [3, 0])).toBe(true);
+    expect(myBoard.placeShip(myShip5, [4, 0])).toBe(true);
+
+    myBoard.printBoard();
+});
+
+test.skip("place a ship, south", () => {
     const myShip = Ship(3);
     const myBoard = Gameboard();
     //ship placement
@@ -11,21 +28,6 @@ test("place a ship, south", () => {
     expect(myBoard.placeShip(myShip, [0, 2])).toBe(false);
 
     //ship out of bounds
-    expect(myBoard.placeShip(myShip, [0, 9])).toBe(false);
-});
-
-test.skip("place a ship test overlap, south", () => {
-    const myShip = Ship(3);
-    const myBoard = Gameboard();
-    myBoard.placeShip(myShip, [1, 0]);
-    myBoard.printBoard();
-    expect(myBoard.placeShip(myShip, [1, 2])).toBe(false);
-    myBoard.printBoard();
-});
-
-test.skip("place a ship test out of bounds, south", () => {
-    const myShip = Ship(3);
-    const myBoard = Gameboard();
     expect(myBoard.placeShip(myShip, [0, 9])).toBe(false);
 });
 
@@ -45,6 +47,49 @@ test.skip("place a ship, west", () => {
 
     //ship out of bounds
     expect(myBoard.placeShip(myShip, [0, 0])).toBe(false);
+
+    myBoard.printBoard();
+});
+
+test.skip("place a ship, north", () => {
+    const myShip = Ship(3);
+    const myBoard = Gameboard();
+    myShip.rotateShip();
+    myShip.rotateShip();
+    //ship placement
+    expect(myBoard.placeShip(myShip, [5, 3])).toBe(true);
+    expect(myBoard.placeShip(myShip, [9, 9])).toBe(true);
+    expect(myBoard.placeShip(myShip, [9, 2])).toBe(true);
+    expect(myBoard.placeShip(myShip, [3, 5])).toBe(true);
+
+    //ship overlap
+    expect(myBoard.placeShip(myShip, [9, 7])).toBe(false);
+
+    //ship out of bounds
+    expect(myBoard.placeShip(myShip, [0, 0])).toBe(false);
+
+    // myBoard.printBoard();
+});
+
+test.skip("place a ship, east", () => {
+    const myShip = Ship(3);
+    const myBoard = Gameboard();
+    myShip.rotateShip();
+    myShip.rotateShip();
+    myShip.rotateShip();
+
+    //ship placement
+    expect(myBoard.placeShip(myShip, [3, 0])).toBe(true);
+    expect(myBoard.placeShip(myShip, [0, 0])).toBe(true);
+    expect(myBoard.placeShip(myShip, [5, 2])).toBe(true);
+    expect(myBoard.placeShip(myShip, [3, 5])).toBe(true);
+    expect(myBoard.placeShip(myShip, [7, 9])).toBe(true);
+
+    //ship overlap
+    expect(myBoard.placeShip(myShip, [9, 0])).toBe(false);
+
+    //ship out of bounds
+    expect(myBoard.placeShip(myShip, [9, 9])).toBe(false);
 
     myBoard.printBoard();
 });
